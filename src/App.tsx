df91a0a12535faa51470d073e845a2a05f5aca49
Etay Ruben <etayruben@gmail.com>
2023-06-11 01:19:27 +0300

    removed the Recipe loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { GET, POST } from "./services/api"
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import RecipiesLoader from './pages/Home/components/recipeLoader';
 import NavigationBar from './pages/NavigationBar';
 import RoutesComponent from './services/Routes';
 
@@ -10,7 +8,6 @@ const App:React.FC = () => {
   return (
     <BrowserRouter>
       <NavigationBar/>
-      <RecipiesLoader/>
       <RoutesComponent/>
     </BrowserRouter>
   );
