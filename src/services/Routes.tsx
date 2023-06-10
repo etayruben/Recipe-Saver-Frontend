672d7df537366e50d21a1294b70e31177d37d4f8
Etay Ruben <etayruben@gmail.com>
2023-06-11 01:25:27 +0300

    added the add-recipe route

--- src/services/Routes.tsx
+++ src/services/Routes.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import RecipiesLoader from '../pages/Home/components/recipeLoader';
-
+import AddRecipe from '../pages/AddRecipe/AddRecipe';
 const RoutesComponent: React.FC = () => {
   return (
     <Routes>
       <Route path="/" Component={RecipiesLoader} />
-      {/* <Route path="/add-recipe" Component={AddRecipe} /> */}
+      <Route path="/add-recipe" Component={AddRecipe} />
       {/* <Route exact path="/about-me" component={AboutMe} /> */}
     </Routes>
   );
