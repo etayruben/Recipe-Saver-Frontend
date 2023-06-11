64840c658dc27c365045ce67c926970efc7eddc6
Etay Ruben <etayruben@gmail.com>
2023-06-11 16:11:51 +0300

    added the error page as a route

--- src/services/Routes.tsx
+++ src/services/Routes.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate, useNavigate } from 'react-router-dom';
 import RecipiesLoader from '../pages/Home/components/recipeLoader';
 import AddRecipe from '../pages/AddRecipe/AddRecipe';
+import ErrorPage from '../pages/Error/Errorpage';
+
 const RoutesComponent: React.FC = () => {
   return (
     <Routes>
       <Route path="/" Component={RecipiesLoader} />
       <Route path="/add-recipe" Component={AddRecipe} />
+      <Route path="/error-page" Component={ErrorPage} />
+      
       {/* <Route exact path="/about-me" component={AboutMe} /> */}
     </Routes>
   );
