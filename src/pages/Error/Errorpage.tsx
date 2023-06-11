8ef77b6013734cbe275ca03e71aa9d3fc701f1c1
Etay Ruben <etayruben@gmail.com>
2023-06-11 16:11:24 +0300

    created a simple error page

---
+++ src/pages/Error/Errorpage.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router";
+import "./Errorpage.css"
+
+const ErrorPage: React.FC = () => {
+    const navigate = useNavigate()
+
+    return (
+        <div className="container">
+        <Typography variant="h4" component="h1" className="errorTitle">
+          Error Page
+        </Typography>
+        <img src="/errorimage.jpeg" alt="Image" />
+        <Typography variant="body1">
+          There was an error while fetching data from the server.
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          className="retryButtons"
+          onClick={() => navigate(-1)}
+        >
+          Retry
+        </Button>
+      </div>
+    );
+  };
+
+  export default ErrorPage
