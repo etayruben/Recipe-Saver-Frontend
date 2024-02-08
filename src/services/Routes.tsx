244c1d7f8cef8700d011cb100434b66cecf24914
Etay Ruben <etayruben@gmail.com>
2024-02-08 22:57:56 +0200

    made some routes protected

--- src/services/Routes.tsx
+++ src/services/Routes.tsx
@@ -3,14 +3,19 @@ import { Route, Routes } from 'react-router-dom';
 import RecipesLoader from '../pages/Home/components/recipeLoader';
 import AddRecipe from '../pages/AddRecipe/components/AddRecipe';
 import ErrorPage from '../pages/Error/components/Errorpage';
+import LoginPage from '../pages/Login/Login';
+import PrivateRoutes from '../components/Common/ProtectedRoute';
 
 const RoutesComponent: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" Component={RecipesLoader} />
-      <Route path="/add-recipe" Component={AddRecipe} />
+      
+      <Route path='/login' Component={LoginPage} />
       <Route path="/error-page" Component={ErrorPage} />
-
+      <Route element={<PrivateRoutes />}>
+        <Route path="/" Component={RecipesLoader} />
+        <Route path="/add-recipe" Component={AddRecipe} />
+      </Route>
       {/* <Route exact path="/about-me" component={AboutMe} /> */}
     </Routes>
   );
